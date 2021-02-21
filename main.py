20015cdcc8e4f738e83b9db112886c0e95da4e83
matej2 <11059438+matej2@users.noreply.github.com>
2021-02-21 17:21:13 +0100

    Flairs

--- main.py
+++ main.py
@@ -1,10 +1,41 @@
 import os
 import praw
+import re
+
 
 class Crossposter:
-    def get_instance(self):
+
+    @staticmethod
+    def get_instance():
         return praw.Reddit(client_id=os.environ['CLIENT_ID'],
                            client_secret=os.environ['CLIENT_SECRET'], password=os.environ['PWD'],
-                           user_agent="testscript", username=os.environ['USER_NAME'])
+                           user_agent="Mention bot", username=os.environ['USER_NAME'])
+
+    @staticmethod
+    def get_discord_subs():
+        subs = os.environ['SUB']
+        re.sub('\\s+', '', subs)
+        return subs.split(",")
+
+    @staticmethod
+    def get_mentions(reddit):
+        return reddit.inbox.mentions(limit=25)
+
+    @staticmethod
+    def get_feed(reddit):
+        return reddit.subreddit("DiscordFeed").new()
+
+    def set_flair(self, submission):
+        choices = submission.flair.choices()
+        template_id = next(x for x in choices if x["flair_text_editable"])["flair_template_id"]
+        submission.flair.select(template_id, 'processed')
+
+
     def main(self):
-        print("hello")
+        r = self.get_instance()
+        feed = self.get_feed(r)
+        for submission in feed:
+            print(f'Processing post: ', submission.title)
+            self.set_flair(submission)
+
+        print(self.get_discord_subs())
