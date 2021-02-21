7bf96da2347da641306c1f3ad9a9e10398cc4c97
matej2 <11059438+matej2@users.noreply.github.com>
2021-02-21 17:45:06 +0100

    Send submissions Comment

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import praw
 import re
 
+from discord_webhook import DiscordWebhook,DiscordEmbed
 
 class Crossposter:
 
@@ -25,17 +26,39 @@ class Crossposter:
     def get_feed(reddit):
         return reddit.subreddit("DiscordFeed").new()
 
-    def set_flair(self, submission):
+    @staticmethod
+    def set_flair(submission):
         choices = submission.flair.choices()
         template_id = next(x for x in choices if x["flair_text_editable"])["flair_template_id"]
         submission.flair.select(template_id, 'processed')
+
+    @staticmethod
+    def send_webhook(url, sub):
+        webhook = DiscordWebhook(url=url)
+        embed = DiscordEmbed(title=sub.title, description='r/DiscordFeed', color=242424)
+        embed.set_image(url=sub.url)
+        webhook.add_embed(embed)
+
+        return webhook.execute()
 
 
     def main(self):
         r = self.get_instance()
         feed = self.get_feed(r)
+        discord_webhooks = self.get_discord_subs()
+
         for submission in feed:
             print(f'Processing post: ', submission.title)
-            self.set_flair(submission)
+
+            if not submission.is_self and submission.link_flair_text != 'processed':
+                for wh in discord_webhooks:
+                    self.send_webhook(wh, submission)
+
+                submission.reply(
+                    f'Submission {submission.title} successfully sent to Discord channels!'
+
+                    f'I am a bot, I read posts from r/DiscordFeed and send them to webhooks. Development is in progress. Please contact me if you want more info.'
+                )
+                self.set_flair(submission)
 
         print(self.get_discord_subs())
